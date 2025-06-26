e804781efa7dfc4a5863ab763c944b83b613313a
lucorcelli <lucorcelli@gmail.com>
2025-06-26 02:45:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ async function buscarParcelas() {
   const response = await fetch(url, {
     headers: {
       "accept": "application/json",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJVU0UiLCJkYzA0MjFkOTc1YWJiNDliNGY3MTIxNzc2ZTc2MmY3ZDVkZmY5MTRlIl0sImp0aSI6IjM4ZmYxYzE0ZTk0NzQzYWRhZGM3NjQ3Mzg1MTJmOTI4IiwibmFtZWlkIjoiZGMwNDIxZDk3NWFiYjQ5YjRmNzEyMTc3NmU3NjJmN2Q1ZGZmOTE0ZSIsImVtYWlsIjoiMDYuMDE0LjU3MS8wMDAxLTYxIiwibmJmIjoxNzUwODk3OTI5LCJleHAiOjE3NTA5ODQzMjksImlhdCI6MTc1MDg5NzkyOX0.XNzLoaqnbi_9irEHEv--4s7u_GHwBUZejLP-0lAXAZU"
-    }
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJVU0UiLCJkYzA0MjFkOTc1YWJiNDliNGY3MTIxNzc2ZTc2MmY3ZDVkZmY5MTRlIl0sImp0aSI6IjM4ZmYxYzE0ZTk0NzQzYWRhZGM3NjQ3Mzg1MTJmOTI4IiwibmFtZWlkIjoiZGMwNDIxZDk3NWFiYjQ5YjRmNzEyMTc3NmU3NjJmN2Q1ZGZmOTE0ZSIsImVtYWlsIjoiMDYuMDE0LjU3MS8wMDAxLTYxIiwibmJmIjoxNzUwODk3OTI5LCJleHAiOjE3NTA5ODQzMjksImlhdCI6MTc1MDg5NzkyOX0.XNzLoaqnbi_9irEHEv--4s7u_GHwBUZejLP-0lAXAZU"    }
   });
 
   const data = await response.json();
