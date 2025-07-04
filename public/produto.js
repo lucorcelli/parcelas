5231d7e8aa143a25123e172bd0f403327fa4dab8
lucorcelli <lucorcelli@gmail.com>
2025-07-03 21:22:19 -0300

    Update produto.js

--- public/produto.js
+++ public/produto.js
@@ -1,5 +1,5 @@
 function abrirJanelaProdutoCompleta(loja, codigoVenda) {
-  const url = `https://integracaodatasystem.useserver.com.br/api/v1/vendas/loja/${loja}/codigo/${codigoVenda}`;
+  const url = `/api/produtoapi?loja=${loja}&codigo=${codigoVenda}`;
   const popup = window.open("", "_blank", "width=720,height=600,left=100,top=100");
 
   popup.document.write(`
